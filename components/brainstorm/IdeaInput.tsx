114dd9dae501dbdfaca8697681c81b9c0eea9efc
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-06-04 22:20:25 -0700

    Revert "Revert "[SSP-207][SSP-208][SSP-209] Setup Brainstorm and History Page…"
    This reverts commit 02a39377eaf38339fee73e64d53123cf59157780.

---
+++ components/brainstorm/IdeaInput.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react'
+import { Animated, SafeAreaView, View } from 'react-native'
+import {
+  TextInput,
+  Button,
+  ActivityIndicator,
+  Icon,
+  Text,
+} from 'react-native-paper'
+import { SPACING } from '@/app/theme'
+import { IdeasTable, LabelsTable, NewIdea, SelectLabel } from '@/db/schema'
+import { db } from '@/db/client'
+import { eq } from 'drizzle-orm'
+import 'react-native-get-random-values'
+import { v4 as uuidv4 } from 'uuid'
+
+// TODO - Make background color, title, and icon, be that project.
+
+const IdeaInput = ({
+  submitCallback,
+  cancelCallback,
+  labelUUID,
+}: {
+  submitCallback: (ideaText: string) => void
+  cancelCallback: () => void
+  labelUUID: string
+}) => {
+  const [ideaText, setIdeaText] = React.useState('')
+  const [label, setLabel] = React.useState<SelectLabel | null>(null)
+  const isFocused = React.useRef(new Animated.Value(0)).current
+
+  const onFocus = React.useCallback(() => {
+    Animated.timing(isFocused, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start()
+  }, [isFocused])
+
+  const onBlur = React.useCallback(() => {
+    Animated.timing(isFocused, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start()
+  }, [isFocused])
+
+  React.useEffect(() => {
+    db.select()
+      .from(LabelsTable)
+      .where(eq(LabelsTable.uuid, labelUUID))
+      .limit(1)
+      .then(r => setLabel(r[0]))
+  }, [labelUUID])
+
+  const handleCancel = React.useCallback(() => {
+    setIdeaText('')
+    cancelCallback()
+  }, [cancelCallback])
+
+  const handleSubmit = React.useCallback(async () => {
+    const idea: NewIdea = {
+      uuid: uuidv4(),
+      text: ideaText,
+      labelId: labelUUID,
+      createdAt: new Date().toISOString(),
+    }
+    const result = await db
+      .insert(IdeasTable)
+      .values(idea)
+      .returning({ uuid: IdeasTable.uuid })
+
+    setIdeaText('')
+    submitCallback(result[0].uuid)
+  }, [submitCallback, ideaText, labelUUID])
+
+  if (label === null) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <ActivityIndicator animating size="large" />
+      </SafeAreaView>
+    )
+  }
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: label.color,
+        justifyContent: 'space-between',
+      }}
+    >
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Icon source={label.icon} size={50} color="white" />
+        <Text>{label.text}</Text>
+      </View>
+      <Animated.View
+        style={{
+          flex: isFocused.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+          }),
+        }}
+      >
+        <TextInput
+          style={{
+            margin: SPACING.md,
+            flex: 1,
+          }}
+          label="Spill it..."
+          value={ideaText}
+          onChangeText={text => setIdeaText(text)}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          multiline
+        />
+      </Animated.View>
+      <View
+        style={{
+          flexDirection: 'row',
+          marginRight: SPACING.md,
+          marginLeft: SPACING.md,
+          marginBottom: SPACING.md,
+        }}
+      >
+        <Button style={{ flex: 1 }} mode="outlined" onPress={handleCancel}>
+          Clear
+        </Button>
+        <Button style={{ flex: 1 }} mode="contained" onPress={handleSubmit}>
+          Submit
+        </Button>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default IdeaInput
---
+++ shared/components/Idea.tsx
@@ -0,0 +1,32 @@
+import { BORDER_RADIUS, SPACING } from '@/app/theme'
+import { View } from 'react-native'
+import { Icon, Text } from 'react-native-paper'
+
+type Props = {
+  color: string
+  icon: string
+  text: string
+  label: string
+}
+
+const Idea = ({ color, icon, text, label }: Props) => {
+  return (
+    <View
+      style={{
+        borderRadius: BORDER_RADIUS.md,
+        flex: 1,
+        backgroundColor: color,
+        padding: SPACING.sm,
+        flexDirection: 'column',
+      }}
+    >
+      <View style={{ flexDirection: 'row' }}>
+        <Icon source={icon} size={24} color="#fff" />
+        <Text style={{ fontWeight: 900, marginLeft: SPACING.sm }}>{label}</Text>
+      </View>
+      <Text>{text}</Text>
+    </View>
+  )
+}
+
+export default Idea

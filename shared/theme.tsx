2e8811cb80dfc4b37f048709629bcd0fce1b2621
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-06-30 12:12:08 -0600

    added colors

--- shared/theme.tsx
+++ shared/theme.tsx
@@ -29,11 +29,95 @@ export const COLORS = {
   },
 }
 
+export const COLORS2 = {
+  NEUTRAL: {
+    '100': '#E3E7E8',
+    '200': '#C7CFD1',
+    '300': '#ABB7BA',
+    '400': '#8F9EA3',
+    '500': '#73868C',
+    '600': '#5C6B70',
+    '700': '#455154',
+    '800': '#2E3638',
+    '900': '#171B1C',
+  },
+  PRIMARY: {
+    '100': '#CCF3FF',
+    '200': '#66DAFF',
+    '300': '#00C1FF',
+    '400': '#007499',
+    '500': '#002733',
+  },
+  SECONDARY: {
+    '100': '#E9CFFC',
+    '200': '#BC6EF7',
+    '300': '#8F0DF2',
+    '400': '#720AC2',
+    '500': '#560891',
+  },
+  SUCCESS: {
+    '100': '#CCFFDD',
+    '200': '#66FF99',
+    '300': '#00FF55',
+    '400': '#00CC44',
+    '500': '#009933',
+  },
+  WARNING: {
+    '100': '#FFF1CC',
+    '200': '#FFD466',
+    '300': '#FFB800',
+    '400': '#996E00',
+    '500': '#332500',
+  },
+  ERROR: {
+    '100': '#FFCCEB',
+    '200': '#FF66C2',
+    '300': '#FF0099',
+    '400': '#CC007A',
+    '500': '#99005C',
+  },
+  MISC: {
+    TRANSPARENT: 'TRANSPARENT',
+    BLACK: '#000',
+    WHITE: '#FFF',
+  },
+  LABELS: {
+    '1': '#FF1A1A',
+    '2': '#FF5F1A',
+    '3': '#FFB905',
+    '4': '#EEFF26',
+    '5': '#73FF31',
+    '6': '#26FFD8',
+    '7': '#33DAFF',
+    '8': '#2DA7FF',
+    '9': '#2562FF',
+    '10': '#6B25FF',
+    '11': '#C425FF',
+    '12': '#FF25CF',
+    '13': '#FF2567',
+    '14': '#FFFFFF',
+    '15': '#A3A3A3',
+  },
+}
+
 export const SPACING = {
   sm: 8,
   md: 16,
 }
 
 export const BORDER_RADIUS = {
-  md: 8,
+  NONE: 0,
+  SMALL: 4,
+  MEDIUM: 8,
+  LARGE: 12,
+}
+
+export const SPACING2 = {
+  XXSMALL: 4,
+  XSMALL: 8,
+  SMALL: 12,
+  MEDIUM: 16,
+  LARGE: 24,
+  XLARGE: 32,
+  XXLARGE: 48,
 }

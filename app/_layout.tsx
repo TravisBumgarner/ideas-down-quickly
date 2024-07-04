7e78889596ff4b04416bdf87353d218b6c42e61c
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-07-04 17:01:55 -0600

    implemented error wrapper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { db } from '@/db/client'
 import migrations from '@/db/migrations/migrations'
 import Toast from '@/shared/components/Toast'
 import Context, { context } from '@/shared/context'
+import * as Sentry from '@sentry/react-native'
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator'
 import { useFonts } from 'expo-font'
 import { Stack, router } from 'expo-router'
@@ -11,6 +12,18 @@ import { useContext, useEffect, useState } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper'
 import 'react-native-reanimated'
+
+Sentry.init({
+  dsn: 'https://64ffbe37c5fcfb045fa5ac415b9e5d16@o196886.ingest.us.sentry.io/4507545983385600',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  _experiments: {
+    // profilesSampleRate is relative to tracesSampleRate.
+    // Here, we'll capture profiles for 100% of transactions.
+    profilesSampleRate: 1.0,
+  },
+})
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
@@ -92,4 +105,4 @@ const AppWrapper = () => {
   )
 }
 
-export default AppWrapper
+export default Sentry.wrap(AppWrapper)

6cfcffe29cc583ba46af986bd1acc1686f4f55b5
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-08-03 15:02:30 -0600

    updated changelog

--- site/src/pages/ChangeLog.tsx
+++ site/src/pages/ChangeLog.tsx
@@ -1,4 +1,4 @@
-import { Description } from '@mui/icons-material'
+import { Description, BugReport } from '@mui/icons-material'
 import {
   Box,
   Container,
@@ -8,18 +8,57 @@ import {
   Typography
 } from '@mui/material'
 import { type FC } from 'react'
-import { pageWrapperCSS } from 'theme'
+import { COLORS, pageWrapperCSS } from 'theme'
+
+enum NoteType {
+  Feature = 'feature',
+  BugFix = 'bugfix'
+}
+
+const noteTypeToIcon = {
+  [NoteType.Feature]: Description,
+  [NoteType.BugFix]: BugReport
+}
+
+const noteTypeToColor = {
+  [NoteType.Feature]: COLORS.SUCCESS['300'],
+  [NoteType.BugFix]: COLORS.ERROR['300']
+} as const
 
 const changelogData: Array<{
   version: string
   date: string
   title: string
-  notes: string[]
+  notes: { type: NoteType, text: string }[]
 }> = [
+    {
+      version: '1.2.0',
+      date: 'August 3rd, 2024',
+      notes: [{
+        type: NoteType.Feature,
+        text: "Replace dropdown for category select/filter with modal"
+      }, {
+        type: NoteType.BugFix,
+        text: "Help messages displaying at the incorrect time"
+      }, {
+        type: NoteType.BugFix,
+        text: "Filter to empty list caused incorrect UI"
+      }, {
+        type: NoteType.Feature,
+        text: "Add feedback button to settings page"
+      }
+      ],
+      title: 'Dropdown Replacement & Bug Fixes'
+    },
     {
       version: '1.1.10',
       date: 'July 18th, 2024',
-      notes: ['Create a Category', 'Create an Idea', 'Reflect on recorded Ideas', "Backup & Restore"],
+      notes: [
+        { type: NoteType.Feature, text: 'Implement Create a Category', },
+        { type: NoteType.Feature, text: 'Implement Create an Idea', },
+        { type: NoteType.Feature, text: 'Implement Reflect on recorded Ideas', },
+        { type: NoteType.Feature, text: "Implement Backup & Restore" },
+      ],
       title: 'Initial Release'
     },
   ]
@@ -37,12 +76,15 @@ const Changelog: FC = () => {
               <Typography variant="h3">{`${entry.version} - ${entry.title}`}</Typography>
               <Typography variant="h4">{entry.date}</Typography>
               <List>
-                {entry.notes.map((note, index) => (
-                  <ListItem key={index}>
-                    <Description sx={{ marginRight: '0.5rem' }} />
-                    <ListItemText primary={note} />
-                  </ListItem>
-                ))}
+                {entry.notes.map((note, index) => {
+                  const Icon = noteTypeToIcon[note.type]
+                  return (
+                    <ListItem key={index}>
+                      <Icon sx={{ marginRight: '0.5rem', color: noteTypeToColor[note.type] }} />
+                      <ListItemText primary={note.text} />
+                    </ListItem>
+                  )
+                })}
               </List>
             </Box>
           ))}

9b02589dea1f5e054f5e72fd8bae626eb7e5974c
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-27 14:20:01 -0600

    implemented

--- site/deploy.sh
+++ site/deploy.sh
@@ -4,18 +4,11 @@ LOCAL_DIR="build/"
 REMOTE_HOST="nfs_ideasdown" # This value comes from ~/.ssh/config
 REMOTE_DIR="/home/public"
 
+echo "Uploading files to $REMOTE_HOST:$REMOTE_DIR"
 rsync -avz --delete "$LOCAL_DIR" "$REMOTE_HOST:$REMOTE_DIR"
+
+# Copy .htaccess file
+echo "Copying .htaccess file to $REMOTE_HOST:$REMOTE_DIR/.htaccess"
+scp .htaccess "$REMOTE_HOST:$REMOTE_DIR/.htaccess"
+
 echo "Files uploaded successfully to $REMOTE_HOST:$REMOTE_DIR"
-
-LARGE_PHOTO_DIR="${HOME}/Desktop/large"
-THUMBNAIL_DIR="${HOME}/Desktop/thumbnail"
-
-if [ -d "$LARGE_PHOTO_DIR" ]; then
-    echo "Uploading large photos..."
-    rsync -avz --delete "$LARGE_PHOTO_DIR/" "$REMOTE_HOST:$REMOTE_DIR/large/"
-fi
-
-if [ -d "$THUMBNAIL_DIR" ]; then
-    echo "Uploading thumbnails..."
-    rsync -avz --delete "$THUMBNAIL_DIR/" "$REMOTE_HOST:$REMOTE_DIR/thumbnail/"
-fi

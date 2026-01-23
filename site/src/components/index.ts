d0a07060dc075372aa9afe28c23a94357379baa0
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2026-01-23 11:40:37 -0600

    remove website (#70)

--- site/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Footer from './Footer'
-import Header from './Header'
-import Router from './Router'
-import Title from './Title'
-
-export { Footer, Header, Router, Title }
-
--- site/src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-import Changelog from './ChangeLog'
-import Contact from './Contact'
-import Error from './Error'
-import LandingPage from './LandingPage'
-import NotFound from './NotFound'
-import PrivacyPolicy from './PrivacyPolicy'
-
-export { Changelog, Contact, Error, LandingPage, NotFound, PrivacyPolicy }
-

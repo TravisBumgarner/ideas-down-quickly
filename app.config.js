8582fe7cd83638738dd9c84a6c4481802868335a
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2026-01-24 20:30:01 -0600

    v1.4.2 (#98)

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ const config = () => ({
     icon: './assets/images/icon.png',
     name: process.env.EXPO_APP_NAME || 'Ideas Down',
     slug: 'ideas-down-quickly',
-    version: '1.4.1',
+    version: '1.4.2',
     orientation: 'portrait',
     scheme: 'myapp',
     userInterfaceStyle: 'automatic',
